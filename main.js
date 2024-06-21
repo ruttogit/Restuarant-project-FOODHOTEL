48d709f29d8ddd74b873473aaf5542dbf4e46e7e
Dennis Kipkoech <kipkoechd09@gmail.com>
2024-06-21 15:31:46 +0300

    changes on mobile responsiveness and addition of togglebar

---
+++ main.js
@@ -0,0 +1,26 @@
+//show and hide menu
+
+
+
+const menu = document.querySelector(".nav-link");
+const menuBtn = document.querySelector("#open-nav");
+const closeBtn = document.querySelector("#close-nav");
+
+
+menuBtn.addEventListener('click', () =>{
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+
+// close nav
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";  
+}
+closeBtn.addEventListener('click', closeNav)
+
+
+
